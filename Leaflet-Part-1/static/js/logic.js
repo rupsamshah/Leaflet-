2284c9928ee2b3cded45fc177cdb11c02974a322
Rupal Shah <rupsamshah@gmail.com>
2023-07-04 01:37:54 -0400

    updated code

---
+++ Leaflet-Part-1/static/js/logic.js
@@ -0,0 +1,98 @@
+const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL
+fetch(queryUrl)
+  .then(response => response.json())
+  .then(earthquakeData => {
+    createFeatures(earthquakeData.features);
+  });
+
+var map = L.map('map').setView([37.8, -96], 5);
+
+// Add the tile layer (OpenStreetMap)
+var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+function createFeatures(earthquakeFeatures) {
+  // Create a layer group for the markers
+  var earthquakes = L.geoJson(earthquakeFeatures, {
+    pointToLayer: createMarker,
+  });
+
+  // Add the earthquake layer to the map
+  earthquakes.addTo(map);
+
+  // Create the legend
+  var legend = L.control({ position: 'bottomright' });
+
+  legend.onAdd = function () {
+    var div = L.DomUtil.create('div', 'info legend');
+    var grades = [-10, 10, 30, 50, 70, 90];
+    var colors = ['#26EC48', '#FDFE0D', '#FFC80D', '#FF7E26', '#FF894D', '#EC1C23'];
+
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+        '<i style="background:' + colors[i] + '"></i> ' +
+        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+  };
+
+  legend.addTo(map);
+}
+
+// Create markers whose size increases with magnitude and color with depth
+function createMarker(feature, latlng) {
+  var marker = L.circleMarker(latlng, {
+    radius: markerSize(feature.properties.mag),
+    fillColor: markerColor(feature.geometry.coordinates[2]),
+    color: "#000",
+    weight: 0.5,
+    opacity: 0.5,
+    fillOpacity: 1
+  });
+
+  // Create a popup with earthquake information
+  var popupContent = `<strong>${feature.properties.title}</strong><br>
+                      Magnitude: ${feature.properties.mag}<br>
+                      Depth: ${feature.geometry.coordinates[2]}`;
+
+  marker.bindPopup(popupContent);
+
+  return marker;
+}
+
+
+// Change marker color based on depth
+function markerColor(depth) {
+  let color = "#B5E61D";
+  switch (true) {
+      case (depth < 10):
+          color = "#03E604";
+          break;
+      case (depth < 30):
+          color = '#A4FE0B';
+          break;
+      case (depth < 50):
+          color = '#B5E61D';
+          break;
+      case (depth < 70):
+          color = '#D5FE0D';
+          break;
+      case (depth < 90):
+          color = '#FF2E15';
+          break;
+  }
+
+
+  return color;
+}
+
+// Define the marker size based on magnitude
+function markerSize(magnitude) {
+  return magnitude * 3;
+}
+
+
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # leaflet-challenge
 
-![1-Logo](C:\Users\rupal\Desktop\USGS.png)
+## Part 1: Create the Earthquake Visualization
+
+    Import and visualize the data by doing the following:
+
+Using Leaflet, create a map that plots all the earthquakes from your dataset based on their longitude and latitude.
+
+Your data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by color. Earthquakes with higher magnitudes should appear larger, and earthquakes with greater depth should appear darker in color.
+
+Hint: The depth of the earth can be found as the third coordinate for each earthquake.
+
+Include popups that provide additional information about the earthquake when its associated marker is clicked.
+
+Create a legend that will provide context for your map data.
